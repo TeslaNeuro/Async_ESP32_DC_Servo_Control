c266c47d883675be7bb5aac5d73e628ae0b1df3c
Arshia Keshvari <110261434+TeslaNeuro@users.noreply.github.com>
2025-03-25 01:16:13 +0000

    Async_Servo_Implementation_Using_Millis()

--- ESP32_Servo_Control.ino
+++ ESP32_Servo_Control.ino
@@ -1,55 +1,54 @@
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
-// called this way, it uses the default address 0x40
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
-// Depending on your servo make, the pulse width min and max may vary, you 
-// want these to be as small/large as possible without hitting the hard stop
-// for max range. You'll have to tweak them as necessary to match the servos you
-// have!
-#define SERVOMIN  125 // this is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX  575 // this is the 'maximum' pulse length count (out of 4096)
+#define SERVOMIN  125 // Minimum pulse length count
+#define SERVOMAX  575 // Maximum pulse length count
 
-// our servo # counter
 uint8_t servonum = 0;
+int angle = 0;
+bool increasing = true;
+unsigned long previousMillis = 0;
+const long interval = 500; // Interval in milliseconds
+
+void moveServo() {
+    pwm.setPWM(0, 0, angleToPulse(angle));
+    pwm.setPWM(3, 0, angleToPulse(angle));
+    Serial.print("Moving to angle: "); Serial.println(angle);
+    
+    if (increasing) {
+        angle += 20;
+        if (angle >= 180) {
+            increasing = false;
+        }
+    } else {
+        angle -= 20;
+        if (angle <= 0) {
+            increasing = true;
+        }
+    }
+}
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println("16 channel Servo test!");
+    Serial.begin(115200);
+    Serial.println("16 channel Servo test!");
 
-  pwm.begin();
-  
-  pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
-
-  //yield();
+    pwm.begin();
+    pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
 }
 
-// the code inside loop() has been updated by Robojax
 void loop() {
-
-
-  for( int angle =0; angle<181; angle +=20){
-    delay(500);
-    pwm.setPWM(0, 0, angleToPulse(angle) );
-    pwm.setPWM(3, 0, angleToPulse(angle) );
-  }
-
-  delay(1000);
- 
+    unsigned long currentMillis = millis();
+    if (currentMillis - previousMillis >= interval) {
+        previousMillis = currentMillis;
+        moveServo();
+    }
 }
 
-/*
- * angleToPulse(int ang)
- * gets angle in degree and returns the pulse width
- * also prints the value on seial monitor
- * written by Ahmad Nejrabi for Robojax, Robojax.com
- */
-int angleToPulse(int ang){
-   int pulse = map(ang,0, 180, SERVOMIN,SERVOMAX);// map angle of 0 to 180 to Servo min and Servo max 
-   Serial.print("Angle: ");Serial.print(ang);
-   Serial.print(" pulse: ");Serial.println(pulse);
-   return pulse;
+int angleToPulse(int ang) {
+    int pulse = map(ang, 0, 180, SERVOMIN, SERVOMAX);
+    Serial.print("Angle: "); Serial.print(ang);
+    Serial.print(" pulse: "); Serial.println(pulse);
+    return pulse;
 }
-
- 
